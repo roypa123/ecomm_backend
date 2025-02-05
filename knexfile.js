bd9b064c59f451ca684235fd7631f41c262914be
Roy P A <roy@nintrivalabs.com>
2025-02-05 15:42:25 +0530

    fix: some changes

--- knexfile.js
+++ knexfile.js
@@ -1,18 +1,3 @@
-// // knexfile.js
-// module.exports = {
-//     client: 'pg',
-//     connection: {
-//       //host: '164.52.200.24',
-//       user: 'postgres',
-//       password: 'password',
-//       database: 'mobikul',
-//     },
-//     migrations: {
-//       tableName: 'knex_migrations',
-//       directory: './migrations',
-//     },
-//   };
-
 module.exports = {
     client: 'pg',
     connection: {
@@ -21,7 +6,6 @@ module.exports = {
       password: 'password',
       database: 'mobikul',
     },
-    //pool: { min: 0, max: 10 },
     migrations: {
       tableName: 'knex_migrations',
       directory: './migrations',
